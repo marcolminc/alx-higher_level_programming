cf7231227482dace84607697cc5652fa357f9ecf
marcolminc <thommarcolminc@gmail.com>
2024-03-12 14:23:25 -0700

    add a check for empty string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(0, len(str)):
-        if 97 <= ord(str[i]) <= 123:
-            print('{:s}'.format(chr(ord(str[i]) - 32)),
+    if not str:
+        print('\n')
+    else:
+        for i in range(0, len(str)):
+            print(chr(ord(str[i]) - 32) if 'a' <= str[i] <= 'z' else str[i],
                   end='' if i < len(str) - 1 else '\n')
-        else:
-            print('{:s}'.format(str[i]), end='' if i < len(str) - 1 else '\n')
