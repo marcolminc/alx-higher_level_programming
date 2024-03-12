1bcb9573d4fb685599eeb09da0b0b5cf02663811
marcolminc <thommarcolminc@gmail.com>
2024-03-12 15:19:20 -0700

    write a function that computes a to the power of b and return the value

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def pow(a, b):
+    result = a
+    if b == 0:
+        return 1
+    elif b < 0:
+        result = 1
+        for i in range(0, -b):
+            result /= a
+    else:
+        for i in range(1, b):
+            result *= a
+    return result
