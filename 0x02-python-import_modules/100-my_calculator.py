4c2bae8c08f8d75ba8842c0d9f624554d96024bc
marcolminc <thommarcolminc@gmail.com>
2024-03-14 16:21:56 -0700

    write a simple command-line interactive calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv, exit
-    if len(argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+    import sys
+    num_args = len(sys.argv) - 1
 
-    a = int(argv[1])
-    b = int(argv[3])
+    if num_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    op = sys.argv[2]
+    # op stands for operator
+    if op != '+' and op != "-" and op != "*" and op != "/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
     from calculator_1 import sub, mul, add, div
 
-    match argv[2]:
-        case '+':
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        case '-':
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        case '*':
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        case '/':
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    if op == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    else:
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
