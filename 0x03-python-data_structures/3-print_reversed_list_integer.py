857be99500aa2da331ba10e36a0fed8e7399686b
marcolminc <thommarcolminc@gmail.com>
2024-03-16 03:17:33 -0700

    use negative len and while loop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for num in my_list:
-        print('{:d}'.format(num))
+    idx = -(len(my_list) - 1)
+    while(idx <= 0):
+        print('{:d}'.format(my_list[-idx]))
+        idx += 1
