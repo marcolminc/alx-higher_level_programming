0831ff9f1d7d9247cb8c26d3f496b4a731a5ec36
marcolminc <thommarcolminc@gmail.com>
2024-03-16 02:04:05 -0700

    write a funcion that prints all integers of alist(of integers)

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,6 @@
 def print_list_integer(my_list=[]):
     if len(my_list) != 0:
         for i in my_list:
-            print("{}".format(i))
+            print("{:d}".format(i))
     else:
         print()
