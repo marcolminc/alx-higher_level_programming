7d32d4a20d9c2c1bc240494e894051ad5c741cf8
marcolminc <thommarcolminc@gmail.com>
2024-03-17 17:47:01 -0700

    remove buffer, instead use size of linked list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,37 +7,34 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int *num_arr, left, right;
-	size_t arr_size;
 	listint_t *current;
+	int left, right, *num_arr;
+	size_t arr_size;
 
 	if (*head == NULL)
 		return (1);
 	current = *head;
-	left = right = 0;
-	arr_size = BUF_SIZE;
+	left = 0, right = 0, arr_size = 0, num_arr = NULL;
+	while (current != NULL)
+		arr_size++, current = current->next;
 	num_arr = malloc(arr_size * sizeof(int));
 	if (num_arr == NULL)
 		return (0);
+	current = *head;
 	while (current != NULL)
 	{
 		num_arr[right++] = current->n;
-		if ((size_t)right >= arr_size)
-		{
-			arr_size *= 2;
-			num_arr = realloc(num_arr, arr_size * sizeof(int));
-			if (num_arr == NULL)
-				return (0);
-		}
 		current = current->next;
 	}
 	right--;
 	while (left <= right)
+	{
 		if (num_arr[left++] != num_arr[right--])
 		{
 			free(num_arr);
 			return (0);
 		}
+	}
 	free(num_arr);
 	return (1);
 }
