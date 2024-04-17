214d06824e177c45e23e85d8d8d615ec9ca6bc34
marcolminc <thommarcolminc@gmail.com>
2024-04-17 10:24:08 -0700

    write a documentatin comment

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+    """
+    read from a file.
+
+    :param filename: The file to be read.
+    :return: Nothing.
+    """
+
     with open(filename, 'r') as file:
         content = file.read()
         print(content, end='')
