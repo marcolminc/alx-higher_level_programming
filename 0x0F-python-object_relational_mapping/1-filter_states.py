a5c067f64fdc4e66fd3c9406b032aeb93931332d
marcolminc <thommarcolminc@gmail.com>
2024-06-21 10:30:18 -0700

    Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -38,10 +38,6 @@ def list_states(username, password, database):
 
 # usage:
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
