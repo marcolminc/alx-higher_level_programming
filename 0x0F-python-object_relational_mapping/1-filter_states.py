6ea5953a9488981751b1b05e7cf547927885da78
marcolminc <thommarcolminc@gmail.com>
2024-06-21 10:27:28 -0700

    Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Module "lists all states with a name starting with N (upper N) from the database
+"""
+import sys
+import MySQLdb
+
+
+def list_states(username, password, database):
+    """
+    lists all states from db: hbtn_0e_0_usa
+
+    Args:
+        username: mysql username
+        password: mysql password
+        database: mysql database
+    """
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+
+
+    db.close()
+
+
+# usage:
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    list_states(username, password, database)
