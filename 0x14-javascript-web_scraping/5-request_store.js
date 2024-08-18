82d4d88cda7847dbacba878de2c4f88ac986df29
marcolminc <thommarcolminc@gmail.com>
2024-08-18 04:28:55 -0700

    Write a script that gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,7 +7,7 @@ req.get(process.argv[2], (e, res, body) => {
   if (e) {
     console.log(e);
   } else {
-    fs.wrteFile(process.argv[3], body, 'utf-8', (e) => {
+    fs.writeFile(process.argv[3], body, 'utf-8', (e) => {
       if (e) {
         console.log(e);
       }
