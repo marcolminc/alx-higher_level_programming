c02017e0eff5bb7b1c378370cfc5ce04b5fb2477
marcolminc <thommarcolminc@gmail.com>
2024-09-04 11:15:07 -0700

    create the database hbtn_0d_2 and the user user_0d_2 who has  only SELECT privilege on the database

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user with only SELECT privilege in the database hbtn_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+ON hbtn_0d_2.* 
+TO 'user_0d_2'@'localhost';
