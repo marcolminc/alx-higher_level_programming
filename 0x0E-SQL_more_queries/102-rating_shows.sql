3da19292aa09261e5d020a4cc7024d675dad10db
marcolminc <thommarcolminc@gmail.com>
2024-09-05 03:59:40 -0700

    write a script tht lists all genres in the db by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- lists all shows from the db by their rating
-SELECT ts.title, COALESCE(SUM(r.rating), 0) AS rating
+SELECT ts.title, COALESCE(SUM(r.rate), 0) AS rating
 FROM tv_shows ts
 LEFT JOIN tv_show_ratings r ON ts.id = r.show_id
 GROUP BY ts.id, ts.title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
