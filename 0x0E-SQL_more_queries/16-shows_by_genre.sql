6b15f5f6cc155dc2d1cd510a19e4a8ca80eb8b59
marcolminc <thommarcolminc@gmail.com>
2024-09-05 03:27:19 -0700

    write a script that lists all shows, all genres linked to the particular show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- write a script that lists all shows and all genres linked to the show
+SELECT ts.title AS show_title, g.name AS genre_name
+FROM tv_shows ts
+LEFT JOIN tv_show_genres sg ON ts.id = sg.show_id
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
+ORDER BY ts.title ASC, g.name ASC;
