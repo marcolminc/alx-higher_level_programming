da65817d736f332f0a88ee7ed392dfa8ef1fc33b
marcolminc <thommarcolminc@gmail.com>
2025-05-13 13:47:42 +0300

    add execution permission

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def main():
-    for i in range(0, 99):
-        print("{:02d}, ".format(i), end='')
-    print(f"{99:02d}")
+    for i in range(0, 100):
+        print(f"{i:02d}", end='')
+        print(", " if i < 99 else '\n', end='')
 
 
 if __name__ == '__main__':
