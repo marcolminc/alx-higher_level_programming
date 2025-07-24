dd8fa58808b76e03dc1ddbf890193f40dbe39399
marcolminc <thommarcolminc@gmail.com>
2025-07-24 17:05:29 +0300

    write a function that prints numbers in fizzbuzz fashion

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print('{}'.format('fizzbuzz'), end='')
+            print('{}'.format('fizzbuzz'), end=' ')
         elif i % 3 == 0:
-            print('{}'.format('fizz'), end='')
+            print('{}'.format('fizz'), end=' ')
         elif i % 5 == 0:
-            print('{}'.format('buzz'), end='')
+            print('{}'.format('buzz'), end=' ')
         else:
-            print('{}'.format(i), end='')
-        if i < 100:
-            print('{}'.format(' '), end='')
+            print('{}'.format(i), end=' ')
