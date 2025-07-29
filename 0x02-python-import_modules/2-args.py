298e9353091a4186bd69309a3e4c98e86a639d9d
marcolminc <thommarcolminc@gmail.com>
2025-07-29 15:04:36 +0300

    write a program that prints argumentss passed to it

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    args = len(sys.argv) - 1
+    print('{} argument'.format(args), end='')
+    if args == 0:
+        print('s.')
+    elif args == 1:
+        print(':')
+    else:
+        print('s:')
+    if args > 0:
+        for i in range(1, args + 1):
+            print('{}'.format(sys.argv[i]))
