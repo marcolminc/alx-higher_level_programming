5eb078c37e7c16c25bc837d8f7f840d5b88a2f57
marcolminc <thommarcolminc@gmail.com>
2025-07-31 01:28:58 +0300

    Write a program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    import sys
 
-if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print('usage: {} <a> <operator> <b>'.format(sys.argv[0]))
-        exit(1)
-    op = sys.argv[2]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if op == '+':
-        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    elif op == '-':
-        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    elif op == '*':
-        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    elif op == '/':
-        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
-    else:
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        sys.exit(1)
+
+    a, op, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+
+    if op not in ('+', '-', '*', '/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    from calculator_1 import add, sub, mul, div
+    operations = {'+': add, '-': sub, '*': mul, '/': div}
+    result = operations[op](a, b)
+
+    print(f"{a} {op} {b} = {result}")
