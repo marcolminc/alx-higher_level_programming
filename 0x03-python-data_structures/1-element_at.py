0a671c32f8e9d9f469157427d986b64bbc44945c
marcolminc <thommarcolminc@gmail.com>
2025-08-01 19:16:22 +0300

    write a function that access an element of a list in a C fashion

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if not my_list:
         return None
     my_len = len(my_list)
-    if idx > my_len:
+    if idx >= my_len:
         return None
     return my_list[idx]
