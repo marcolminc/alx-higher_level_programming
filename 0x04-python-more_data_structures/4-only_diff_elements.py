aa50af33705b3a85433c4fbd6f35258d4426bd90
marcolminc <thommarcolminc@gmail.com>
2025-08-05 09:39:05 +0300

    consider differences from both sets and join them

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1.difference(set_2)
+    diff_1 = set_1.difference(set_2)
+    diff_2 = set_2.difference(set_1)
+    return diff_1.union(diff_2)
