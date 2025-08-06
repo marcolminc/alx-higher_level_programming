2283e28fb55efb2eab7aaf74659c4e8edc528ee5
marcolminc <thommarcolminc@gmail.com>
2025-08-06 15:00:30 +0300

    Write a function that raises a type exception.

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def raise_exception():
+    """Raises a type exception.
+
+    This function is designed specifically to demonstrate exception raising.
+    Arguments:
+        None.
+    Raises:
+        TypeError: Always raised when this function is called
+    Returns:
+        None.
+    """
+    raise TypeError
