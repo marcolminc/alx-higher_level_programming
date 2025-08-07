2785643b2dd6bbbc076e0964e0d55cdbc29a7339
marcolminc <thommarcolminc@gmail.com>
2025-08-07 18:17:11 +0300

    include module level doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""A module that defines a Square class.
+This module contains the implementation of a basic geometric shape, square,
+representing the first step in Python object-oriented programming journey.
+"""
 class Square:
-    """Defines a geometric shape: square.
+    """A class that represents a geometric square.
 
-    This class represents creation of the very first class in the Python OOP
-    journey.
+    This square class serves as the foundation for learning Python's
+    object-oriented programming concepts. Future implementation may include
+    attributes like size and methods for area, circumference calculation.
+
+    Attributes:
+        None currently.
     """
     pass
