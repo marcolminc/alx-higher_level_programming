6ccb05b5944a51d07cc78525015572d7042d1cea
marcolminc <thommarcolminc@gmail.com>
2025-08-07 18:07:59 +0300

    write an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a geometric shape: square.
+
+    This class represents creation of the very first class in the Python OOP
+    journey.
+    """
+    pass
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,3 +1,4 @@
 # 0x06. Python - Classes and Objects
 Table of contents:
+1. 0. My first square
 
