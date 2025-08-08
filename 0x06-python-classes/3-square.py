368dd8149f7a9b28d1b67ffc7c0ebb984e8e965a
marcolminc <thommarcolminc@gmail.com>
2025-08-08 12:40:07 +0300

    write a class that defines a square class based on 2-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""A module that defines a square class based on 1-square.py
+
+This class initializes size with optional value which must be an integer
+greater than zero.
+"""
+
+
+class Square:
+    """A class that represents a geometric square.
+
+    It initializes size with an optional value which must be an integer
+    greater than zero. It also has a method to compute area.
+    Attributes:
+        __size: The length of each side of the square. This attribute is
+        private and should not be accessed directly
+    """
+    def __init__(self, size=0):
+        """Initializes a new Square instance with the given size.
+
+        The initialization method sets up the fundamental property of the
+        square shape. The size value is stored in a private attribute to
+        enforce encapsulation. Size must be an integer greater than zero
+
+        Args:
+            size: The length of each side of the square. Must be positive
+            integral value.
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    """Returns the current square area.
+
+    Args:
+        None
+    """
+    def area(self):
+        return self.__size ** 2
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -3,4 +3,5 @@ Table of contents:
 1. 0. My first square
 2. 1 Square with size
 3. 2. Size validation
+4. 3. Area of a square
 
