93492cd3385ab0c71ceffc451e81427ee8091d22
marcolminc <thommarcolminc@gmail.com>
2026-05-23 04:25:05 -0700

    document the module

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""Locked out class - prevention of dynamic instance attr creation.
+
+This module creates a locked out class that prevents creation of
+dynamic instance attributes except the one called first_name.
+"""
+
+
 class LockedClass:
+    """An empty class locked out of dynamic instance attributes."""
+
     __slots__ = ['first_name']
